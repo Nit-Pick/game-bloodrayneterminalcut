e19ede92094ec56a2e0e9e916ea377c92797efa7
Pickysaurus <pickysaurus@googlemail.com>
2021-01-17 15:55:33 +0000

    Removed chunks of code that will not be needed.

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 //Import some assets from Vortex we'll need.
 //Required Stuff
 const path = require('path');
-const { actions, fs, DraggableList, FlexLayout, MainPage, log, selectors, util } = require('vortex-api');
-const winapi = require('winapi-bindings');
-const React = require('react');
-const BS = require('react-bootstrap');
-const { remote } = require('electron');
+const { actions, fs,  log, selectors, util } = require('vortex-api');
 //End Required Stuff
 
 //Parser Constants
-const IniParser = require('vortex-parse-ini');
-const parser = new IniParser.default(new IniParser.WinapiFormat());
 const BRTC_INI = 'PCPODCustom.INI';
 //End Parser Constants
-
-const { connect } = require('react-redux');
 
 //Game Details
 const GAME_ID = 'bloodrayneterminalcut';
@@ -23,9 +15,8 @@ const GOGAPP_ID = '1598751450';
 const GAME_NAME = 'Bloodrayne Terminal Cut';
 //End Game Details
 
-const MOD_FILE_EXT = ".zip";
-
-const I18N_NAMESPACE = `game-${GAME_ID}`;
+const MOD_FILE_EXT = ".POD";
+const BASE_PODS = ['PCART.POD', 'PCART2.POD', 'PCMODEL.POD', 'PCSET.POD', 'PCSOUND.POD', 'STARTUP.POD', 'WORLD.POD'];
 
 function main(context) {
 	//This is the main function Vortex will run when detecting the game extension.
@@ -41,153 +32,22 @@ function main(context) {
 		requiredFiles: [
 		'STARTUP.POD',
 		'rayne1.exe'
-    ],
-    setup: prepareForModding,
-    environment: {
-      SteamAPPId: STEAMAPP_ID,
-    },
-    details: {
-      steamAppId: STEAMAPP_ID,
-      gogAppId: GOGAPP_ID,
-    },
-});
-context.registerInstaller('bloodrayneterminalcut-mods', 25, testSupportedContent, installContent);
-	
-context.registerMainPage('sort-none', 'Load Order', LoadOrder, {
-	id: 'bloodrayneterminalcut-load-order',
-	hotkey: 'E',
-	group: 'per-game',
-	visible: () => selectors.activeGameId(context.api.store.getState()) === GAME_ID,
-	props: () => ({
-		t: context.api.translate,
-	}),
-});	
+		],
+		setup: prepareForModding,
+		environment: {
+		SteamAPPId: STEAMAPP_ID,
+		},
+		details: {
+		steamAppId: STEAMAPP_ID,
+		gogAppId: GOGAPP_ID,
+		},
+	});
+	context.registerInstaller('bloodrayneterminalcut-mods', 25, testSupportedContent, installContent);
+		
 	return true;
 }
 
-function loadOrderPrefix(api, mod) {
-	const state = api.store.getState();
-	const gameProfile = selectors.lastActiveProfileForGame(state, GAME_ID);
-	if (gameProfile === undefined) {
-		return 'ZZZZ-';
-	}
-	const profile = selectors.profileById(state, gameProfile);
-	const loadOrder = util.getSafe(state, ['persistent', 'loadOrder', profile?.id], []);
-	
-	const pos = loadOrder.indexOf(mod.id);
-	if (pos === -1) {
-		return 'ZZZZ-';
-	}
-	return makePrefix(pos) + '-';
-}
 
-
-function modIsEnabled(props, mod) {
-	return (!!props.modState[mod])
-	? props.modState[mod].enabled
-	: false;
-}
-
-function LoadOrderBase(props) {
-	const loValue = (input) => {
-		const idx = props.order.indexOf(input);
-		return idx !== -1 ? idx : props.order.length;
-	}
-	const filtered = Object.keys(props.mods).filter(mod => modIsEnabled(props, mod));
-	const sorted = filtered.sort((lhs, rhs) => loValue(lhs) - loValue(rhs));
-	
-	class ItemRenderer extends React.Component {
-		render() {
-			const item = this.props.item;
-			const index = (props.order.indexOf(item) === -1)
-			? loadOrderPrefix(_API, item)
-			: props.order.indexOf(item);
-			
-			return !modIsEnabled(props, item)
-			? null
-			: React.createElement(BS.ListGroupItem, {
-				style: {
-					backgroundColor: 'var(--brand-bg, black)',
-					borderBottom: '2px solid var(--border-color, white)'
-				},
-			},
-			React.createElement('div', {
-				style: {
-					fontSize: '1.1em',
-				},
-			},
-			React.createElement('img', {
-				src: props.mods[item].attributes.pictureUrl
-				? props.mods[item].attributes.pictureUrl
-				: `${__dirname}/gameart.jpg`,
-				className: 'mod-picture',
-				width:'75px',
-				height:'45px',
-				style: {
-					margin: '5px 10px 5px 5px',
-					border: '1px solid var(--brand-secondary,#D78F46)',
-				},
-			}),
-			util.renderModName(props.mods[item])));
-		}
-	}
-	return React.createElement(MainPage, {},
-	React.createElement(MainPage.Body, {},
-	React.createElement(BS.Panel, { id: 'bloodrayneterminalcut-loadorder-panel' },
-	React.createElement(BS.Panel.Body, {},
-	React.createElement(FlexLayout, { type: 'row' },
-	React.createElement(FlexLayout.Flex, {},
-	React.createElement(DraggableList, {
-		id: 'bloodrayneterminalcut-loadorder',
-		itemTypeId: 'bloodrayneterminalcut-loadorder-item',
-		items: sorted,
-		itemRenderer: ItemRenderer,
-		style: {
-			height: '100%',
-			overflow: 'auto',
-			borderWidth: 'var(--border-width, 1px)',
-			borderStyle: 'solid',
-			borderColor: 'var(--border-color, white)',
-		},
-		apply: ordered => {
-			props.onSedDeploymentNecessary(props.profile.gameId, true);
-			return props.onSetOrder(props.profile.id, ordered)
-		},
-	})
-	),
-	React.createElement(FlexLayout.Flex, {},
-	React.createElement('div', {
-		style: {
-			padding: 'var(--half-gutter, 15px)',
-		}
-	},
-	React.createElement('h2', {},
-	props.t('Changing your load order', { ns: I18N_NAMESPACE })),
-	React.createElement('p', {},
-	props.t('Drag and drop the mods on the left to reorder them. Bloodrayne Terminal Cut loads mods in decending order so Vortex prefixes '
-	+ 'the directory names with "AAA, AAB, AAC, ..." to ensure they load in the order you set here. '
-	+ 'Mods placed at the top of the load order will have priority over those below them.', { ns: I18N_NAMESPACE })),
-	React.createElement('p', {}, 
-	props.t('Note: You can only manage mods installed with Vortex. Installing other mods manually may cause unexpected errors.', { ns: I18N_NAMESPACE })),
-	))
-	)))));
-}
-
-function findGame() {
-	try {
-		const instPath = winapi.RegGetValue(
-		'HKEY_LOCAL_MACHINE',
-		'SOFTWARE\\WOW6432Node\\GOG.com\\Games\\' + GOGAPP_ID,
-		'PATH');
-		if (!instPath) {
-			throw new Error('empty registry key');
-		}
-    return Promise.resolve(instPath.value);
-  } catch (err) {
-    return util.GameStoreHelper.findByAppId([STEAMAPP_ID, GOGAPP_ID])
-      .then(game => game.gamePath);
-  }
-}
 
 //Create INI
 function onGameModeActivated(gameId, api) {
@@ -209,22 +69,6 @@ function onGameModeActivated(gameId, api) {
       });
 
 }
-
-function createINI(iniLocation, api) {
-    // Creates a new PCPODCustom.INI with the default settings
-    const defaultData = 
-        '[Archive]\n' +
-        'sResourceArchive2List=\n' +
-        'sResourceDataDirsFinal=STRINGS\\\n' +
-        'bInvalidateOlderFiles=1\n';
-
-    return fs.writeFileAsync(iniLocation, defaultData).then(() => iniLocation)
-    .catch((err) => {
-        api.sendNotification({id:'bloodrayneterminalcut-ini-create-error', type: 'error', title: 'Could not create PCPODCustom.INI', message: `${err.code} - ${err.message}`});
-        return '';
-    });
-}
-const BASE_PODS = ['PCART', 'PCART2', 'PCMODEL', 'PCSET', 'PCSOUND', 'STARTUP', 'WORLD'];
 
 function writeLoadOrder(iniPath, modPODs) {
 	// Use a set to ensure there are no duplicate entries
@@ -274,25 +118,6 @@ function installContent(files) {
 
   return Promise.resolve({ instructions });
 }
-
-function mapStateToProps(state) {
-	const profile = selectors.activeProfile(state);
-	return {
-		profile,
-		modState: util.getSafe(profile, ['modState'], {}),
-		mods: util.getSafe(state, ['persistent', 'mods', profile.gameId], []),
-		order: util.getSafe(state, ['persistent', 'loadOrder', profile.id], []),
-	};
-}
-
-function mapDispatchToProps(dispatch) {
-	return {
-		onSedDeploymentNecessary: (gameId, necessary) => dispatch(actions.setDeploymentNecessary(gameId, necessary)),
-		onSetOrder: (profileId, ordered) => dispatch(actions.setLoadOrder(profileId, ordered)),
-	};
-}
-
-const LoadOrder = connect(mapStateToProps, mapDispatchToProps)(LoadOrderBase);
 
 module.exports = {
     default: main,
